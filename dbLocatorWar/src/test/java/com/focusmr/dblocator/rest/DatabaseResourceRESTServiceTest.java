ae0c5a77d7bbe8b38ad3e559bbb289a4c39b42cd
Radim Pavlicek <Radim.Pavlicek@focusmr.com>
2013-03-11 11:19:06 +0100

    test constructor

---
+++ dbLocatorWar/src/test/java/com/focusmr/dblocator/rest/DatabaseResourceRESTServiceTest.java
@@ -0,0 +1,12 @@
+package com.focusmr.dblocator.rest;
+
+import org.junit.Test;
+
+/**
+ */
+public class DatabaseResourceRESTServiceTest {
+    @Test
+    public void testPublicConstructor() {
+        new DatabaseResourceRESTService();
+    }
+}
